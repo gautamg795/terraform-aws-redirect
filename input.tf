8c84188a04f35c4de1b83ad7b1069c71746cdc11
Aidan Feldman <aidan.feldman@gsa.gov>
2020-03-24 10:34:07 -0400

    truncate the bucket prefix to fit

--- input.tf
+++ input.tf
@@ -10,5 +10,7 @@ variable "redirect_to" {
 locals {
   records      = "${keys(transpose(var.domains))}"
   redirect_to = "${replace(var.redirect_to, "/^(?:https?://)?/", "https://")}"
-  bucket_name = "redirect-${replace(replace(local.redirect_to, "https://", ""), "/\\W/", "")}"
+  # truncate to 37 characters to fit the bucket_prefix limit
+  # https://groups.google.com/d/msg/terraform-tool/hhC5P4eIcVU/rdWiVfRvLgAJ
+  bucket_name = "${format("%.37s", "redirect-${replace(replace(local.redirect_to, "https://", ""), "/\\W/", "")}")}"
 }
